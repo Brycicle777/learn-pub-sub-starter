8f52626f2d298fd1a64fbb10f4e31db6b32d25e9
Bryce Hargis <brycehargis@outlook.com>
2025-02-13 16:29:22 -0800

    add basic json publish

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"internal/pubsub"
+	"internal/routing"
 	"log"
 	"os"
 	"os/signal"
@@ -17,6 +19,17 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Connection successful.")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Error creating connection channel: %v", err)
+	}
+	//don't forget about "peril_direct exchange needed in RabbitMQ web config"
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Error publishing message: %v", err)
+	}
+
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	exit := <-signalChan
--- go.mod
+++ go.mod
@@ -1,5 +1,13 @@
 module github.com/bootdotdev/learn-pub-sub-starter
 
-go 1.22.1
+go 1.23.6
 
 require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+
+require internal/pubsub v0.0.0
+
+replace internal/pubsub => ./internal/pubsub
+
+require internal/routing v0.0.0
+
+replace internal/routing => ./internal/routing
