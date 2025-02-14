c0e3a95567eb52497d2162e11800528dc361c43c
Bryce Hargis <brycehargis@outlook.com>
2025-02-13 16:07:54 -0800

    add conn and interupt handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("Error dialing amqp server: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection successful.")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	exit := <-signalChan
+	log.Fatal("Got signal: ", exit)
+
 }
